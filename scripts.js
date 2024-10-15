aa53a2db3fb158545c74e8ebe79a8bccace31db7
Geovane Girard <g.ng2009@hotmail.com>
2024-10-15 11:10:26 -0300

    Adicionando itens a lista e alterando entre comprado e não comprado, através do seletor além de alterar o item de container atrávés do appendChild

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
 let contador = 0;
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
@@ -24,6 +25,22 @@ function adicionarItem(evento) {
     const checkboxLabel = document.createElement("label");
     checkboxLabel.setAttribute("for", checkboxInput.id);
 
+    checkboxLabel.addEventListener("click", function (evento) {
+        const checkboxInput = evento.currentTarget.querySelector(".input-checkbox");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo")
+        if (checkboxInput.checked) {
+            checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else {
+            checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista)
+        }
+        
+})
+
     const checkboxCustomizado = document.createElement("div");
     checkboxCustomizado.classList.add("checkbox-customizado");
 
@@ -34,6 +51,7 @@ function adicionarItem(evento) {
     containerNomeDoItem.appendChild(containerCheckbox)
 
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
     containerNomeDoItem.appendChild(nomeDoItem)
 
